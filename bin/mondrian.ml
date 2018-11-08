dd5c7f99d2e881a77a5b554bda6f60b7d233bcb4
Alexandre Moine <alexandre@moine.me>
2018-11-08 12:26:32 +0100

    Loop

--- bin/mondrian.ml
+++ bin/mondrian.ml
@@ -1,4 +1,16 @@
+open Graphics
+
+let rec loop () =
+  let e = wait_next_event  [ Mouse_motion ; Key_pressed ] in
+  if e.keypressed
+  then
+    match e.key with
+    | 'q' -> ()
+    | _ -> loop ()
+  else loop ()
+
 let main () =
-  print_endline "Mondrian"
+  open_graph " 500x500" ;
+  loop ()
 
 let _ = main ()
