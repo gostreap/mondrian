74e03f533e78212e2629615a0fc032f7d2b04607
Alexandre Moine <alexandre@moine.me>
2018-11-08 16:11:14 +0100

    Add change_color

--- lib/bsp.ml
+++ lib/bsp.ml
@@ -1,10 +1,15 @@
+open Graphics
+
 type label =
   { coord : int;
     colored : bool;
   }
 
-type bsp = R of Graphics.color option
+(* Commence avec une séparation verticale *)
+type bsp = R of color option
          | L of label * bsp * bsp
+
+type point = (int * int)
 
 (* Affichage rudimentaire de BSP *)
 let rec string_of_bsp (bsp : bsp) =
@@ -13,7 +18,7 @@ let rec string_of_bsp (bsp : bsp) =
      begin
        match x with
          None -> "None"
-       | Some x -> if x = Graphics.red then "red" else "blue"
+       | Some x -> if x = red then "red" else "blue"
      end
   | L (lab,l,r) ->
     "(" ^ (string_of_bsp l) ^ ") " ^ (string_of_int (lab.coord)) ^ " (" ^ (string_of_bsp r) ^ ")"
@@ -26,7 +31,7 @@ NOTE: Pour l'instant, toutes les arrêtes sont visibles
 let random_bsp_naive (profondeur : int) (largeur : int) (hauteur : int) =
   let rec aux profondeur largeur hauteur v =
   if profondeur = 0
-  then R (Some (if Random.bool () then Graphics.blue else Graphics.red))
+  then R (Some (if Random.bool () then blue else red))
   else
     let lab =
       { coord = Random.int (if v then largeur else hauteur);
@@ -36,3 +41,19 @@ let random_bsp_naive (profondeur : int) (largeur : int) (hauteur : int) =
     let r = aux (profondeur-1) (if v then (largeur-lab.coord) else largeur) (if v then hauteur else (hauteur - lab.coord)) (not v) in
     L (lab,l,r)
   in aux profondeur largeur hauteur true
+
+
+(* Change la couleur d'un rectangle d'un bsp, dans lequel se situe p *)
+let rec change_color (bsp : bsp) (p : point) (c : color) =
+  let rec aux bsp (x,y) c v =
+    match bsp with
+    | R _ -> R (Some c)
+    | L (lab, left, right) ->
+       if v
+       then if x < lab.coord
+            then L (lab, aux left p c (not v), right)
+            else L (lab, left, aux right (x-lab.coord,y) c (not v))
+       else if y < lab.coord
+            then L (lab, aux left p c (not v), right)
+            else L (lab, left, aux right (x,y-lab.coord) c (not v))
+  in aux bsp p c true
