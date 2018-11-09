f05550ac195ce5c3244a7f6f030ed089302c624b
Alexandre Moine <alexandre@moine.me>
2018-11-09 08:31:01 +0100

    Better bsp.mli

--- lib/bsp.mli
+++ lib/bsp.mli
@@ -1,8 +1,8 @@
-type bsp
-type point
-   
-val string_of_bsp : bsp -> string
+type label = { coord : int; colored : bool; }
+type bsp = R of Graphics.color option | L of label * bsp * bsp
+type point = int * int
 
-val random_bsp_naive : ?v:bool -> ?start_larg:int -> ?start_haut:int -> int -> int -> int -> bsp
-                                              
+val string_of_bsp : bsp -> string
+val random_bsp_naive :
+  ?v:bool -> ?start_larg:int -> ?start_haut:int -> int -> int -> int -> bsp
 val change_color : ?v:bool -> bsp -> point -> bsp
