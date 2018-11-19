ca53f4fae6a05cf173638d1a6ae325d78c9757d0
Alexandre Moine <alexandre@moine.me>
2018-11-19 18:18:42 +0100

    Fix mli

--- lib/generate_formule.mli
+++ lib/generate_formule.mli
@@ -2,3 +2,5 @@ open Bsp
 open Formule
 
 val get_fnc_of_bsp : int -> bsp -> formule option
+val generate_config : int * int * int -> int * int * int -> int list -> (lit * lit) list list
+val generate_all_config : Couleur.couleur -> int -> int -> int -> int -> int list -> (lit * lit) list list
