4c0649d387cccab7b89526e9d01b6f3a04013035
Alexandre Moine <alexandre@moine.me>
2018-11-20 19:45:25 +0100

    Remove an other intermediate list

--- lib/generate_formule.ml
+++ lib/generate_formule.ml
@@ -37,21 +37,22 @@ let rev_map_ap f l xs =
   let rec rmap_f accu = function
     | [] -> accu
     | a::l -> rmap_f (f a :: accu) l
-  in
-rmap_f xs l
+  in rmap_f xs l
 
 let generate_config (r,g,b) (rs,gs,bs) list =
   let red = get_n_tuples_in_list (r-rs) list in
   let mkredform = rev_map_ap (fun n -> choose Red n) in
   let green = List.map (fun x -> (get_n_tuples_in_list (g-gs) (get_compl x list))) red in
   let mkgreenform =  List.map (fun n -> choose Green n) in
-  let blue = List.map2 (fun r x -> List.map (fun g -> (get_n_tuples_in_list (b-bs) (get_compl (List.rev_append r g) list))) x) red green in
+  let mkblue r x = List.map (fun g -> (get_n_tuples_in_list (b-bs) (get_compl (List.rev_append r g) list))) x in
   let mkblueform = rev_map_ap (fun n -> choose Blue n) in
   let greenblueform = List.map2
-                        (List.map2
-                           (fun g -> let g = mkgreenform g in List.map (fun b -> mkblueform b g))
+                        (fun g r ->
+                          List.map2
+                            (fun g -> let g = mkgreenform g in List.map (fun b -> mkblueform b g))
+                             g (mkblue r g)
                         )
-                        green blue in
+                        green red in
   let form = List.map2 (fun r x -> List.map (fun y -> List.map (fun z -> mkredform r z) y) x) red greenblueform in
   form |> concat_term |> concat_term
 
