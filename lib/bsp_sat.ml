a3df04b75cedbae8eb09fe3541c0bfd294771626
Alexandre Moine <alexandre@moine.me>
2018-12-11 15:44:35 +0100

    other pass

--- lib/bsp_sat.ml
+++ lib/bsp_sat.ml
@@ -42,41 +42,32 @@ let rec color_bsp_sat_line (bsp_sat : [< `Blue | `Red | `Green] bsp_sat) (linetr
 let bsp_sat_of_working_bsp (working_bsp : [< `Blue | `Red | `Green] bsp) (linetree :  [< `Blue | `Red | `Green] linetree) =
   let rec aux v bsp =
     match bsp with
-    | R x ->
-       begin
-       match x with
-         | None -> (v+1, R_sat(v, false, None))
-         | Some x -> (v+1, R_sat(v, true, Some x))
-       end
     | L (_,l,r) ->
        let (n,ll) = aux v l in
        let (m,rr) = aux n r in
        (m,L_sat (None, false, ll, rr))
-  in
-  (* print_endline (string_of_bsp_sat (color_bsp_sat_line (snd (aux 0 working_bsp)) linetree)); *)
-  color_bsp_sat_line (snd (aux 0 working_bsp)) linetree
+    | R x ->
+       match x with
+         | None -> (v+1, R_sat(v, false, None))
+         | Some x -> (v+1, R_sat(v, true, Some x))
+  in  color_bsp_sat_line (snd (aux 0 working_bsp)) linetree
 
 (* Renvoie un bsp ou les feuilles ont un indice différent de 1 si leur couleur est fixé *)
 let rec secure_bsp_sat (bsp :  [< `Red | `Green | `Blue] bsp_sat) =
-    match bsp with
+  match bsp with
+  | R_sat _ as r -> r
   | L_sat (c,_,l,r) ->
-     let lr =
-       match l, r with
-       | R_sat (n,_,co), R_sat (m,_,_) -> Some (L_sat (c,true,R_sat (n,true,co),R_sat (m,true,co)))
-       | L_sat (_,b,_,_) as l, (L_sat (_,b',_,_) as r) ->
-          if b && b'
-          then Some (L_sat (c,true,l,r))
-          else None
-       | _ -> None
-
-     in
-     if maybe false (switch_coul_l false false false false (fun _ -> true)) c
-     then
-       match lr with
-       | Some x -> x
-       | _ -> L_sat (c,false,secure_bsp_sat l,secure_bsp_sat r)
-     else L_sat (c,false,secure_bsp_sat l,secure_bsp_sat r)
-  | i -> i
+      let isnone = L_sat (c,false,secure_bsp_sat l,secure_bsp_sat r) in
+      if maybe false (switch_coul_l false false false false (fun _ -> true)) c
+      then
+        match l, r with
+        | R_sat (n,_,co), R_sat (m,_,_) -> L_sat (c,true,R_sat (n,true,co),R_sat (m,true,co))
+        | L_sat (_,b,_,_) as l, (L_sat (_,b',_,_) as r) ->
+           if b && b'
+           then L_sat (c,true,l,r)
+           else isnone
+        | _ -> isnone
+      else isnone
 
 let rec loop_sat (n : int) (b : [< `Red | `Green | `Blue] bsp_sat) =
   if n <= 0 then b
@@ -89,26 +80,26 @@ let rec loop_sat (n : int) (b : [< `Red | `Green | `Blue] bsp_sat) =
  * list est la liste des rectangles non sécurisés*)
 let get_adja_stat (bsp_sat :  [< `Red | `Green | `Blue] bsp_sat) =
   (* print_endline "début adja stat"; *)
-  let rec get_stat ?(v=true) (is_l : bool) (bsp_sat :  [< `Red | `Green | `Blue] bsp_sat)
+  let rec get_stat v (is_l : bool) (bsp_sat :  [< `Red | `Green | `Blue] bsp_sat)
           : (int * int * int * int list) =
     match bsp_sat with
     | L_sat (_,_,x,y) ->
-       let rx,gx,bx,ll as x' = get_stat ~v:(not v) is_l x in
-       let ry,gy,by,lr as y' = get_stat ~v:(not v) is_l y in
+       let rx,gx,bx,ll as x' = get_stat (not v) is_l x in
+       let ry,gy,by,lr as y' = get_stat (not v) is_l y in
        if not v
        then if is_l then y' else x'
        else (rx+ry,gx+gy,bx+by,ll@lr)
     | R_sat (n,s,c) ->
        if s then
-           let (r,g,b) = maybe (0,0,0) (switch_coul (1,0,0) (0,1,0) (0,0,1)) c in
-           (r,g, b, [])
+         let (r,g,b) = maybe (0,0,0) (switch_coul (1,0,0) (0,1,0) (0,0,1)) c in
+         (r,g, b, [])
        else (0,0,0,[n])
   in
   match bsp_sat with
   | R_sat _ -> (0,0,0,[])
   | L_sat (_,_,l,r) ->
-     let (lr,lg,lb,llist) = get_stat true l in
-     let (rr,rg,rb,rlist) = get_stat false r in
+     let (lr,lg,lb,llist) = get_stat true true l in
+     let (rr,rg,rb,rlist) = get_stat true false r in
      (lr+rr,lg+rg,lb+rb,llist@rlist)
 
 let check_line (line :  [< `Red | `Green | `Blue] bsp_sat) =
@@ -124,10 +115,7 @@ let check_line (line :  [< `Red | `Green | `Blue] bsp_sat) =
      | Some Cyan -> (r < size/3 || r = 0) && g <= size/2 && b <= size/2
      | Some White -> r <= size/3 && g <= size/3 && b <= size/3
      | Some (C co) ->
-        let borne =
-          if size mod 2 = 1 then size/2+1
-          else size/2
-        in
+        let borne = if size mod 2 = 1 then size/2+1 else size/2 in
         match co with
         | `Red -> g < borne && b < borne
         | `Green -> r < borne && b < borne
